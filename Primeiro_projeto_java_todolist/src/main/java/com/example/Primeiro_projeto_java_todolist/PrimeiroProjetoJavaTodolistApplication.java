b99e47d618e42e9f740c15a50a513bb1f94f6c4a
Matheus Borges <matheuslb2003@gmail.com>
2023-10-09 19:57:36 -0300

    configuração inicial do projeto to-do list

---
+++ Primeiro_projeto_java_todolist/src/main/java/com/example/Primeiro_projeto_java_todolist/PrimeiroProjetoJavaTodolistApplication.java
@@ -0,0 +1,13 @@
+package com.example.Primeiro_projeto_java_todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjetoJavaTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProjetoJavaTodolistApplication.class, args);
+	}
+
+}
---
+++ Primeiro_projeto_java_todolist/src/test/java/com/example/Primeiro_projeto_java_todolist/PrimeiroProjetoJavaTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Primeiro_projeto_java_todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoJavaTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
